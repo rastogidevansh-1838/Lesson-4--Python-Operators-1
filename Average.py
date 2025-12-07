fd0756cc7b1e5bee34a1692e5027df88462e3665
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2025-12-07 10:07:23 +0530

    first commit

---
+++ Average.py
@@ -0,0 +1,9 @@
+tree1 = 47
+tree2 = 68
+tree3 = 89
+tree4 = 23
+tree5 = 46
+sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of all 5 trees is:", sum)
+average = sum/5
+print("The average of all trees is:", average)
